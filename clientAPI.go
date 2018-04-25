c5b740fa2681d7612f35292abc1399ecf6ad894f
Chris Garland <chris.garland@vgw.co>
2018-04-25 12:34:51 +0800

    master - enable CORS

--- clientAPI.go
+++ clientAPI.go
@@ -10,6 +10,7 @@ import (
 
 func GetClients(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	json.NewEncoder(w).Encode(SelectClients())
 }
 
